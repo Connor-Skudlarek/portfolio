60742188c998ac9ecb8f74b2c9a68cb81dec6e78
Connor Skudlarek <connor.skudlarek@gmail.com>
2023-06-15 15:36:48 -0700

    Add skills cards and some minor styling.

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,11 +1,42 @@
-import '../styles/Skills.css'
+import "../styles/Skills.css";
 
-function Skills(){
-    return (
-        <div>
-            This is the Skills section.
-        </div>
-    )
+function Skills() {
+  return (
+    <div class="Skills-container">
+      <div class="Skills-card">
+        <img class="Skills-logo" alt="HTML logo"></img>
+        <p class="Skills-skill">HTML</p>
+      </div>
+      <div class="Skills-card">
+        <img class="Skills-logo" al="CSS logo"></img>
+        <p class="Skills-skill">CSS</p>
+      </div>
+      <div class="Skills-card">
+        <img class="Skills-logo" alt="JavaScript logo"></img>
+        <p class="Skills-skill">JavaScript</p>
+      </div>
+      <div class="Skills-card">
+        <img class="Skills-logo" alt="NodeJS logo"></img>
+        <p class="Skills-skill">Node</p>
+      </div>
+      <div class="Skills-card">
+        <img class="Skills-logo" alt="ExpressJS logo"></img>
+        <p class="Skills-skill">Express</p>
+      </div>
+      <div class="Skills-card">
+        <img class="Skills-logo" alt="ReactJS logo"></img>
+        <p class="Skills-skill">React</p>
+      </div>
+      <div class="Skills-card">
+        <img class="Skills-logo" alt="MongoDB logo"></img>
+        <p class="Skills-skill">MongoDB</p>
+      </div>
+      <div class="Skills-card">
+        <img class="Skills-logo" alt="Git logo"></img>
+        <p class="Skills-skill">Git/GitHub</p>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
