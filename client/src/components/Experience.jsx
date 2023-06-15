7241c0ccdebd447f85385fd23bc2b22a2538817f
Connor Skudlarek <connor.skudlarek@gmail.com>
2023-06-15 15:36:16 -0700

    Add lorem ipsum content to expereince section.

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,11 +1,61 @@
-import '../styles/Experience.css'
+import "../styles/Experience.css";
 
-function Experience(){
-    return (
-        <div>
-            This is the Experience section.
-        </div>
-    )
+function Experience() {
+  return (
+    <div>
+      This is the Experience section.
+      <p Style="margin: 200px;">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        quis ornare ante, sit amet pellentesque sapien. Suspendisse eu ex sit
+        amet ante tincidunt dapibus. Nullam sit amet ultrices elit. Aenean
+        tempus tincidunt enim et vehicula. Nulla aliquam odio arcu, in vulputate
+        erat pulvinar vitae. Donec porttitor ex ac enim vestibulum ullamcorper.
+        In nibh est, suscipit a venenatis in, ornare eget justo. Sed sed lacus
+        et ante consequat blandit. Nunc eget vehicula arcu. Suspendisse euismod
+        ante sed neque porttitor cursus. Aliquam sit amet nulla sit amet libero
+        sollicitudin vestibulum eu non dui.
+      </p>
+      <p Style="margin: 200px;">
+        Praesent sit amet porttitor nibh, quisvenenatis ex. Suspendisse
+        scelerisque pulvinar erat id mollis. Cras id elit neque. Donec tincidunt
+        libero quis velit scelerisque maximus. Aliquam erat volutpat. Ut nec
+        dictum ipsum. Morbi vel massa in ex imperdiet consectetur. Aliquam erat
+        volutpat. Morbi vestibulum eros sit amet risus facilisis, et condimentum
+        metus tempor. Cras interdum justo non urna mattis scelerisque. Quisque
+        lobortis felis quam, sit amet hendrerit velit lacinia ac. Suspendisse
+        enim ipsum, euismod a nisi eu, bibendum pretium ipsum.
+      </p>
+      <p Style="margin: 200px;">
+        Aliquam erat volutpat. Morbi interdum finibus ligula, in vulputate diam
+        rutrum sit amet. Aliquam in feugiat mauris. Sed sollicitudin risus sem,
+        et convallis odio condimentum ac. Proin consectetur risus vel lacus
+        viverra ultrices. Duis id turpis arcu. Vivamus luctus mattis felis, eget
+        rhoncus urna commodo quis. Integer sed metus nunc. Etiam tincidunt sem
+        quis ante maximus, ut malesuada arcu ultricies. Nullam dui magna,
+        dignissim quis justo et, auctor tristique mauris. Sed consequat nisl ut
+        libero molestie, eget condimentum risus tincidunt. Morbi mollis sed
+        augue ac pharetra. Nunc ornare dignissim tortor, non mattis ante
+        eleifend ut. Proin porttitor tortor eu massa porta, eget tincidunt enim
+        porta.
+      </p>
+      <p Style="margin: 200px;">
+        Curabitur vel ullamcorper mauris, sit amet consequat libero. Sed arcu
+        elit, porta in diam sit amet, rhoncus iaculis est. Duis euismod tellus
+        et nisi pharetra porta a a elit. Nullam eget rutrum turpis. Nam aliquet
+        blandit mollis. In hac habitasse platea dictumst. Aliquam viverra eget
+        lorem sed commodo. Vivamus maximus risus eget lorem rhoncus, sed
+        consectetur risus accumsan. Vivamus lacinia suscipit nunc nec
+        pellentesque. In mi erat, tristique sed blandit bibendum, vulputate quis
+        erat. Suspendisse pharetra nunc elit. Morbi egestas enim eget sapien
+        efficitur elementum. Duis volutpat, nisl eu dictum lacinia, orci velit
+        hendrerit mauris, at tempor libero velit quis libero. Nam nec egestas
+        urna. Sed eleifend erat vitae massa lobortis condimentum. Mauris vel est
+        auctor, luctus eros lacinia, aliquam purus. Nulla consectetur egestas
+        arcu, sit amet tincidunt nibh varius sed. Mauris quis lectus porta,
+        consectetur magna vel, sodales dui.
+      </p>
+    </div>
+  );
 }
 
 export default Experience;
