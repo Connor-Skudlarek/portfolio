cd7a011cf2c07dd2216d094839c1fbab3929ade1
Connor-Skudlarek <connor.skudlarek@gmail.com>
2024-05-19 13:27:20 -0700

    Add some changes to config and css

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -2,16 +2,16 @@ import "../styles/Landing.css";
 import Profile from "./Profile";
 function Landing() {
   return (
-    <div className="section0">
-      <div className="Landing-container">
+    <div className="bg-[#f5f5dc80]">
+      <div className="flex flex-col items-center max-w-prose font-[1.6rem] p-[20px] my-0 m-auto md:justify-between md:flex-row md:px-[150px] md:py-[40px] md:items-start">
         <Profile />
-        <div className="Landing-about-me">
-          <h2 className="Landing-short-about-me">
+        <div className="flex flex-col ml-[50px] md:text-left">
+          <h2 className="">
             Hello there! My name is Connor Skudlarek, and I'm a full-stack web
             developer.
           </h2>
-          <hr className="Landing-about-me-break" />
-          <p className="Landing-long-about-me">
+          <hr className="w-[150px]" />
+          <p className="">
             If you found yourself on this page, you probably just want to see my
             work and know what I can do.
           </p>
