f9adc83b645fa5ec491fbfb004eba58ed3d5477a
Connor Skudlarek <connor.skudlarek@gmail.com>
2024-06-27 23:34:58 -0700

    I don't even know anymore, why did I do T3 instead of just running vanilla?

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+
+export default async function Home() {
+  return (
+    <main className="container">
+      <Header />
+      <Hero />
+    </main>
+  );
+}
